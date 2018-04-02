16f8d8fb29189cafdda5dd1124da8ce05506d094
Jakub Zelenka <bukka@php.net>
2018-04-02 15:18:07 +0100

    Test error case in CreateAction

--- instance/action_test.go
+++ instance/action_test.go
@@ -12,15 +12,31 @@ func TestCreateAction(t *testing.T) {
 		e string
 	}{
 		{"test", BaseAction{typeName: "test"}, ""},
+		{1, nil, "Invalid Action type"},
+		{map[string]interface{}{"Type": "test"}, BaseAction{typeName: "test"}, ""},
+		{map[string]interface{}{"NoType": "test"}, nil, "Action does not have Type field"},
+		{map[string]interface{}{"Type": 1}, nil, "Action Type has to be a string"},
 	}
 
 	for _, table := range tables {
-		a, _ := CreateAction(table.v)
-		if reflect.TypeOf(a) != reflect.TypeOf(table.t) {
-			t.Errorf("The expected struct type of action is %T, instead %T returned", table.t, a)
+		a, e := CreateAction(table.v)
+		at, tt := reflect.TypeOf(a), reflect.TypeOf(table.t)
+		if at != tt {
+			t.Errorf("The expected struct type of action is %s, instead %s returned", tt, at)
 		}
-		if a.Type() != table.t.Type() {
-			t.Errorf("The expected type of action is %s, instead %s returned", table.t.Type(), a.Type())
+		if e == nil {
+			if table.e != "" {
+				t.Errorf("Expected error '%s' but no error returned", table.e)
+			}
+			if a.Type() != table.t.Type() {
+				t.Errorf("The expected type of action is %s, instead %s returned",
+					table.t.Type(), a.Type())
+			}
+		} else {
+			if e.Error() != table.e {
+				t.Errorf("Expected error '%s' but instead error '%s' returned",
+					e.Error(), table.e)
+			}
 		}
 	}
 
