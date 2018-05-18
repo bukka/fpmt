92fdaef6e278466aca52c92abaa797915fde4a63
Jakub Zelenka <bukka@php.net>
2018-05-18 21:23:35 +0100

    Add the main body for TestCreateSettings

--- instance/settings_test.go
+++ instance/settings_test.go
@@ -1,7 +1,36 @@
 package instance
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCreateSettings(t *testing.T) {
+	tables := []struct {
+		c *SettingsConfig
+		s *Settings
+		e string
+	}{
+		{nil, nil, ""},
+	}
 
+	for _, table := range tables {
+		s, e := CreateSettings(table.c)
+		if e != nil {
+			if table.e == "" {
+				t.Errorf("Expected no error but instead error '%s' returned",
+					e.Error())
+			} else if e.Error() != table.e {
+				t.Errorf("Expected error '%s' but instead error '%s' returned",
+					table.e, e.Error())
+			}
+			continue
+		}
+		if table.e != "" {
+			t.Errorf("Expected error '%s' but no error returned", table.e)
+		}
+		if !reflect.DeepEqual(s, table.s) {
+			t.Errorf("The settings %s does not match expected %s", s, table.s)
+		}
+	}
 }
